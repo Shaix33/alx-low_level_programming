be8095bd031226a4459d987da8a23f2912cd9238
Shaix33 <KevinNdlondlo@gmail.com>
2022-06-09 06:54:40 -0700

    4. isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
 /**
  * _isalpha - Function prototype
  * Description: Checks if a character is a letter
- *
+ * @c ascii character
  * Return: 1 if it is a letter otherwise 0
  */
 int _isalpha(int c)
--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -1,3 +1,6 @@
+#include "main.h"
+#include <ctype.h>
+
 /**
  * main - check the code.
  *
