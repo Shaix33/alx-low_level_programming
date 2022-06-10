a1c4bfa9c727ce55f2076ac3787ff7c247a99b2f
Shaix33 <KevinNdlondlo@gmail.com>
2022-06-10 12:31:41 -0700

    Syncing changes

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - Function prototype
+ * print_numbers() - Function prototype
  * Description: Prints the numbers, from 0 to 9, followed by a new line
  * Return: void
  */
--- 0x04-more_functions_nested_loops/5-main.c
+++ 0x04-more_functions_nested_loops/5-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_numbers() - check the code
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	print_numbers();
+	more_numbers();
 	return (0);
 }
