e5e6b50b1040dd13a564e7e490fe6fdb1a5779d9
Shaix33 <KevinNdlondlo@gmail.com>
2022-06-13 05:12:01 -0700

    changing folder name

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98(int *n) - Function prototype
+ * Description: Takes a pointer to an int and updates value it points to to 98
+ * followed by a new line
+ * Return: void
+ */
+
+int reset_to_98(char *s)
+{
+
+
+}
+	
