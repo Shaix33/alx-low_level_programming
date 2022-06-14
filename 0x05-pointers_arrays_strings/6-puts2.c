46e35075d60dbe8540dc2894d13de62d771b56ba
Shaix33 <KevinNdlondlo@gmail.com>
2022-06-14 11:23:17 -0700

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a  string to stdout
+ * @str: string to print
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int x = 0;
+
+	while (*(str + x))
+	{
+		_putchar(*(str + x));
+		x = x + 2;
+	}
+	_putchar('\n');
+}
