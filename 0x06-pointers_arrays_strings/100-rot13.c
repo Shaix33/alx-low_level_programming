1cf9a1317a58d0e9145fe07a0b8f3685ebe59d61
Shaix33 <KevinNdlondlo@gmail.com>
2022-07-07 10:59:54 -0700

    8. rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13: Function prototype
+ * @src: pointer to the string to encode
+ * Description: encodes a string using rot13, replacing a character with the 13th letter after  * it
+ * Return: pointer to encoded src
+ */
+
+char *rot13(char *src)
+{
+	int x, c;
+	char alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (x = 0; src[x]; x++)
+	{
+		for (c = 0; c < 54; c++)
+		{
+			if (((src[x] > 'z' || src[x] < 'a') && (src[x] > 'Z' || src[x] < 'A'))
+					|| src[x] != alphabet[c])
+				continue;
+
+			src[x] = rot13[c];
+			break;
+		}
+	}
+	return (src);
+}
