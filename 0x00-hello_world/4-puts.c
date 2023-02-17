10fe829978721df5563c750bfdfbb08c875d7f24
Shaix33 <KevinNdlondlo>
2023-02-17 13:05:14 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - program entry point
+ * description - a program that prints out a string
+ * Return: void
+ */
+int main(void)
+{
+	char str[] = "Programming is like building a multilingual puzzle\n";
+
+	puts(str);
+
+	return (0);
+}
