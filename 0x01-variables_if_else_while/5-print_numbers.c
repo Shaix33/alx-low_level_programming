b2571078872b3c96581aca7d102387e7a322e38b
Shaix33 <KevinNdlondlo>
2023-02-21 11:05:26 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description - prints base 10 numbers
+ * Return: void
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
