af0b2d640595648b50ec777cec4b4bc79eeb0b7c
Shaix33 <KevinNdlondlo>
2023-03-14 01:18:52 -0700

    adding files

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * create_array - creates an array of chars, and initializes it with a specific char.
+ * @size: size of the array
+ * @c: array initializer
+ * Return: new array
+ */
+char *create_array(unsigned int size, char c)
+{
