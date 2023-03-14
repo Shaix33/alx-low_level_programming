f74d4e3d35f5806f20f57b5aeee246110c0bf268
Shaix33 <KevinNdlondlo>
2023-03-14 15:32:39 -0700

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,5 +6,7 @@
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 char *create_array(unsigned int size, char c);
+int **alloc_grid(int width, int height);
+void print_grid(int **grid, int width, int height);
 
 #endif
